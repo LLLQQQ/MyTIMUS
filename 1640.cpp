d0f58a8035493bde13b9f0778cda241508611b18
lllqqq <1170494354@qq.com>
2017-09-07 10:35:46 +0100

    1640AC

--- 1640.cpp
+++ 1640.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
-int n,x[101],y[101];
+int n, x[101], y[101];
+int sq(int a, int b)
+{
+    return a * a + b * b;
+}
 int main()
 {
-    int i;
+    int i, r = 0, sx = 1001, sy = 1001, t;
     cin >> n;
-    for (i=0;i<n;i++){
-        cin>>x[i]>>y[i];
+    for (i = 0; i < n; i++)
+    {
+        cin >> x[i] >> y[i];
+        t = sq(x[i] - sx, y[i] - sy);
+        if (t > r)
+        {
+            r = t;
+        }
     }
-    
-    return 0；
+    cout << sx << " " << sy << " ";
+    cout << setiosflags(ios::fixed) << setprecision(9) << sqrt(r) << endl;
+    return 0;
 }
