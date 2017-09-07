95a7db55f311029073bc788481a6aef2b2c2d2d4
lllqqq <1170494354@qq.com>
2017-09-07 17:03:09 +0100

    1335AC

---
+++ 1335.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int n;
+int main()
+{
+    int a, b, c;
+    cin >> n;
+    cout << n * n << " " << n * n + n + 1 << " " << n * n + 1 << endl;
+    return 0;
+}
