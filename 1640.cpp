6f470f4eb60bc741ec02fcb41ae488073b3ee168
lllqqq <1170494354@qq.com>
2017-09-07 09:45:17 +0100

    1640update

---
+++ 1640.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int n,x[101],y[101];
+int main()
+{
+    int i;
+    cin >> n;
+    for (i=0;i<n;i++){
+        cin>>x[i]>>y[i];
+    }
+    
+    return 0；
+}
